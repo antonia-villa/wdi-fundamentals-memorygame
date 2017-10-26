7bca1d340bf71dc401f214a60ecb71a2a33e0297
Antonia <av.fractalwork@gmail.com>
2017-10-26 14:27:34 -0400

    created game logic

--- wdi-fundamentals-memorygame/memory_game/js/main.js
+++ wdi-fundamentals-memorygame/memory_game/js/main.js
@@ -31,22 +31,49 @@ var checkForMatch = function() {
 	}
 }
 
-var flipCard = function(cardId) {
+// Allow user to flipCard
+var flipCard = function () {
+  // Select appropriate card
+  cardId = this.getAttribute('data-id');
+  console.log(cardId);
 
-	console.log("User flipped " + cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
+  // Append card to array of cards
+  cardsInPlay.push(cards[cardId].card);
 
-	cardsInPlay.push(cards[cardId].suit);
+  // selete the source image for the card
+  this.setAttribute('src', cards[cardId].cardImage);
 
-	if (cardsInPlay.length === 2) {
-		checkForMatch();
-	}
+  // Check of cards in play and check for a match
+  if (cardsInPlay.length === 2) {
+    // If match occurs, call function
+    checkForMatch();
+    // clear the array for the next round
+    cardsInPlay = [];
+  }
+};
+
+// Create Board function 
+var createBoard = function () {
+  // create elements for cards on the board
+  for (var i = 0; i < cards.length; i++) {
+    // Create image element to represent each card
+    var cardElement = document.createElement('img');
+
+    // Assign the appropriate image source to represent the back of the card
+    cardElement.setAttribute('src', 'images/back.png');
+
+	// Set the card's 'data-id' attribute to be the index of the current element
+    cardElement.setAttribute('data-id', i);
+
+	// Create event lister for when a user clicks a card
+    cardElement.addEventListener('click', flipCard);
+
+    // Append the card to the game board
+    document.getElementById('game-board').appendChild(cardElement);
+
+  }
 
 }
 
-flipCard(0);
-flipCard(2);
-
-
-
+// Create the board by calling the CreatBoad function
+createBoard();
